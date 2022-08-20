dddc0ff527adf7ba863a12b137637bf80fe07918
prasannagithub035@gmail.com <prasannagithub035@gmail.com>
2022-08-20 23:06:34 +0530

    Working with sql lite DB

--- index.py
+++ index.py
@@ -1,10 +1,25 @@
 import streamlit as st
 import pandas as pd
+import sqlite3
+conn = sqlite3.connect("samples.db", check_same_thread=False)
+curs = conn.cursor()
 
-st.text("Welcome to streamlit App")
-if st.button('Say hello'):
-    st.write('Why hello there')
-if st.button("Clicked"):
+st.text("Welcome to login page")
+user = st.text_input("enter the user")
+pasw = st.text_input("enter password")
 
-    st.write('Goodbye')
+
+def update_db(users, passws):
+    curs.execute("create table if not exists account(name text(50), password text(50))")
+    curs.execute("insert into account values (?,?)", (users, passws))
+    conn.commit()
+
+
+if st.button('login'):
+    if user !=None:
+
+        update_db(user, pasw)
+        st.write('correct user')
+    else:
+        st.write("wrong")
 
